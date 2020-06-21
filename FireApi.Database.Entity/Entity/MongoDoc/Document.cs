386041d5c358b84dc695d85dbe4faca67f565b32
Daxter44 <marcinek44@windowslive.com>
2020-06-21 15:23:53 +0200

    * Document.cs: * IDocument.cs: * BsonCollectionAttribute.cs:
    * FireApi.Database.Entity.csproj: Added mongoDB Document clases

---
+++ FireApi.Database.Entity/Entity/MongoDoc/Document.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace FireApi.Database.Entity.MongoDoc
+{
+    public abstract class Document : IDocument
+    {
+        public ObjectId Id { get; set; }
+
+        public System.DateTime CreatedAt => Id.CreationTime;
+    }
+}
---
+++ FireApi.Database.Entity/Entity/MongoDoc/IDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FireApi.Database.Entity.MongoDoc
+{
+    public interface IDocument
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        ObjectId Id { get; set; }
+
+        System.DateTime CreatedAt { get; }
+    }
+}
